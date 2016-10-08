9276b5c943b3fde1588d1bc4e8e2da912eb1a81f
vanporman <vanporman@gmail.com>
2016-10-08 08:14:36 +0300

    added 2 new print out lines

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 	public static void main(String[] args){
 		System.out.println("Hello World!!");
-		System.out.println("Hello uus rida lisatud githubis!!");
+		System.out.println("Mängu autor: Andreas");
+		System.out.println("Aasta: 2016");
 	}
 }
